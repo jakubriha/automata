1864a35993f4a0275eef54c45464fee1b5a607c9
Jakub Riha <jakubriha@outlook.com>
2017-02-18 16:17:48 +0100

    Remove fwa variable naming

--- test-suite/Tests/Operations/DeterminizeSpec.hs
+++ test-suite/Tests/Operations/DeterminizeSpec.hs
@@ -10,17 +10,17 @@ import Operations (charsToSymbols, determinize, run)
 
 spec :: Spec
 spec =
-  describe "Fwa" $ do
+  describe "fa" $ do
     describe "defined in 2.txt" $ do
 
       it "accepts 'a a' after determinization" $
-        assertFa (testDirectory ++ "2.txt") (\fwa -> run (determinize fwa) (charsToSymbols "aa"))
+        assertFa (testDirectory ++ "2.txt") (\fa -> run (determinize fa) (charsToSymbols "aa"))
 
       it "accepts 'a b' after determinization" $
-        assertFa (testDirectory ++ "2.txt") (\fwa -> run (determinize fwa) (charsToSymbols "ab"))
+        assertFa (testDirectory ++ "2.txt") (\fa -> run (determinize fa) (charsToSymbols "ab"))
 
       it "doesn't accept 'b b' after determinization" $
-        assertFa (testDirectory ++ "2.txt") (\fwa -> not $ run (determinize fwa) (charsToSymbols "bb"))
+        assertFa (testDirectory ++ "2.txt") (\fa -> not $ run (determinize fa) (charsToSymbols "bb"))
 
 testDirectory =
   "test-suite/AutomataExamples/"
