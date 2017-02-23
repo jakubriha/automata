3331a47c380ee197733fa375e7cf72b30b1b4020
Jakub Riha <jakubriha@outlook.com>
2017-02-23 18:36:47 +0100

    Implement isSubsetOf

---
+++ test-suite/Tests/Operations/IsSubsetOfSpec.hs
@@ -0,0 +1,19 @@
+module Tests.Operations.IsSubsetOfSpec
+  ( spec
+  ) where
+
+import Test.Hspec
+import Test.QuickCheck
+
+import Tests.Common (assert2Fa)
+import Operations (isSubsetOf)
+
+spec :: Spec
+spec =
+  describe "FA" $ do
+      it "defined in 0.txt is subset of FA defined in oneStateFa.txt" $
+        assert2Fa "0.txt" "oneStateFa.txt" isSubsetOf
+
+      it "defined in 1.txt is subset of FA defined in 0.txt" $
+        assert2Fa "1.txt" "0.txt" isSubsetOf
+
