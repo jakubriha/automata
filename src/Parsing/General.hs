f3060a3a65321e1a42367f5a7d3a2f971a6452b3
Jakub Riha <jakubriha@outlook.com>
2017-03-01 17:21:16 +0100

    Parse multiple initial states

--- src/Parsing/General.hs
+++ src/Parsing/General.hs
@@ -3,9 +3,10 @@ module Parsing.General
   , loadFa
   ) where
 
-import Data.ByteString as B
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
 
-import Data.Set as Set
+import qualified Data.Set as Set
 
 import Types.Fta as Fta
 import Types.Fa as Fa
@@ -20,31 +21,25 @@ loadFa :: (Monad m) => FilePath -> IO (m (Fa Fa.Symbol Fa.State))
 loadFa filePath =
   fmap parseFa (B.readFile filePath)
 
-parseFa :: (Monad m) => B.ByteString -> m (Fa Fa.Symbol Fa.State)
+parseFa :: (Monad m) => ByteString -> m (Fa Fa.Symbol Fa.State)
 parseFa fileContent =
   case parseFta fileContent of
     Left error -> fail error
-    Right fta -> case ftaToFa fta of
-      Just fa -> return fa
-      _ -> error "Cannot convert FTA to fa"
+    Right fta -> return $ ftaToFa fta
 
-ftaToFa :: Fta -> Maybe (Fa Fa.Symbol Fa.State)
+ftaToFa :: Fta -> Fa Fa.Symbol Fa.State
 ftaToFa (Fta states finalStates transitions rankedAlphabet) =
-  fmap mapper (findInitialStateIn transitions)
+  Fa (extractInitialStates transitions) (Set.toList finalStates) (Set.toList faTransitions)
     where
       faTransitions = (Set.map ftaToFaTransition . Set.filter isNotInitialTransition) transitions
-      mapper initialState = Fa [initialState] (Set.toList finalStates) (Set.toList faTransitions)
 
 isNotInitialTransition :: Fta.Transition -> Bool
 isNotInitialTransition = not . Set.null . inputStates
 
 ftaToFaTransition :: Fta.Transition -> Fa.Transition Fa.Symbol Fa.State
 ftaToFaTransition (Fta.Transition symbol inputStates finalState) =
-  Fa.Transition symbol (elemAt 0 inputStates) finalState
+  Fa.Transition symbol (Set.elemAt 0 inputStates) finalState
 
-findInitialStateIn :: Set Fta.Transition -> Maybe Fta.State
-findInitialStateIn transitions =
-  case findSingleInSet (Set.null . inputStates) transitions of
-    Just x -> Just (Fta.finalState x)
-    _ -> Nothing
-
+extractInitialStates :: Set.Set Fta.Transition -> [Fta.State]
+extractInitialStates =
+  fmap Fta.finalState . Set.toList . Set.filter (Set.null . inputStates)
