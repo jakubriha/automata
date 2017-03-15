d9ba59d5bbc147ebf468ec336fa73cb0ad4c465e
Jakub Riha <jakubriha@outlook.com>
2017-03-15 11:29:14 +0100

    Visualize Fa

---
+++ src/Visualization.hs
@@ -0,0 +1,68 @@
+module Visualization
+  ( displayFa
+  ) where
+
+import Graphics.UI.Gtk
+import Control.Monad.IO.Class
+import System.Directory (getTemporaryDirectory, removeFile, doesFileExist)
+import System.Process (callProcess)
+import System.IO (hClose, openTempFile)
+import Types.Fa
+
+displayFa :: (Show sym, Show sta) => Fa sym sta -> IO ()
+displayFa fa = do
+  tempDirectory <- getTemporaryDirectory
+  (dotFilePath, dotFilehandle) <- openTempFile tempDirectory "automata.gv"
+  (imageFilePath, imageFilehandle) <- openTempFile tempDirectory "automata.png"
+  hClose dotFilehandle
+  hClose imageFilehandle
+  saveDotToImageFile dotFilePath imageFilePath (faToDot fa)
+  displayImage imageFilePath
+  removeFile dotFilePath
+  removeFile imageFilePath
+
+saveDotToImageFile :: FilePath -> FilePath -> String -> IO ()
+saveDotToImageFile dotFilePath imageFilePath dotContent = do
+  writeFile dotFilePath dotContent
+  callProcess "dot" ["-Tpng", dotFilePath, "-o", imageFilePath]
+
+faToDot :: (Show sym, Show sta) => Fa sym sta -> String
+faToDot (Fa _ _ transitions) =
+  "digraph something {" ++ faTransitionsToDot transitions ++ "}"
+    where
+      faTransitionsToDot =
+        concatMap mapper
+
+      mapper (Transition symbol state finalState) =
+        showInQuotes state ++ " -> " ++ showInQuotes finalState ++ " [ label=" ++ show symbol ++ " ];"
+
+showInQuotes :: Show a => a -> String
+showInQuotes object =
+  "\"" ++ content ++ "\""
+    where
+      content = (filter (/= '\"') . show) object
+
+displayImage :: FilePath -> IO ()
+displayImage imagePath = do
+  imageExists <- doesFileExist imagePath
+  if not imageExists
+    then putStrLn $ "Cannot find file " ++ imagePath
+    else do
+      initGUI
+      window <- windowNew
+      window `on` deleteEvent $ liftIO mainQuit >> return False
+      scroll <- scrolledWindowNew Nothing Nothing
+      scrolledWindowSetPolicy scroll PolicyAutomatic PolicyAutomatic
+      vbox <- vBoxNew False 0
+      hbox <- hBoxNew False 0
+      set window [ windowDefaultWidth := 500
+        , windowDefaultHeight := 500
+        , containerChild := vbox
+        , containerBorderWidth := 0 ]
+      image <- imageNew
+      boxPackEnd vbox hbox PackNatural 0
+      boxPackStart vbox scroll PackGrow 0
+      scrolledWindowAddWithViewport scroll image
+      imageSetFromFile image imagePath
+      widgetShowAll window
+      mainGUI
