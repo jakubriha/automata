186c907856d4d989e47417672574040451e1b198
Jakub Riha <jakubriha@outlook.com>
2017-05-02 13:43:55 +0200

    Support benchmarking unary functions

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,9 @@ module Main where
 import Parsing.General (parseFa)
 import Types.Fa (Fa)
 import qualified Data.ByteString as B
-import Operations.Antichain.Inclusion (isSubsetOf)
+import qualified Operations.Antichain.Inclusion as Inclusion
+import qualified Operations.Antichain.Universality as Universality
+import qualified Operations.Regular as Regular
 import System.Environment (getArgs)
 import Control.DeepSeq
 import Control.Exception (evaluate)
@@ -16,43 +18,74 @@ main = do
   arguments <- getArgs
   case parseArguments arguments of
     Nothing -> putStrLn "Wrong arguments"
-    Just (filePath1, filePath2) -> do
-      file1 <- B.readFile filePath1
-      file2 <- B.readFile filePath2
-      case parseFa file1 of
+    Just filePath -> do
+      file <- B.readFile filePath
+      case parseFa file of
         Left error -> fail error
-        Right fa1 ->
-          case parseFa file2 of
-            Left error -> fail error
-            Right fa2 ->
-              benchmarkForce performOperation fa1 fa2 >>= print
-
-parseArguments :: [String] -> Maybe (FilePath, FilePath)
-parseArguments (x:y:_) =
-  Just (x, y)
+        Right fa ->
+          benchmarkForce Universality.isUniversal fa >>= print
+          
+parseArguments :: [String] -> Maybe FilePath
+parseArguments [x] =
+  Just x
 parseArguments _ =
   Nothing
 
--- http://stackoverflow.com/questions/18612303/haskell-how-to-benchmark-a-computation-accurately-with-deepseq-force
-
-benchmarkForce :: (NFData a, NFData p, NFData r) => (p -> r -> a) -> p -> r -> IO a
-benchmarkForce action param1 param2 = do
+benchmarkForce :: (NFData a, NFData p) => (p -> a) -> p -> IO a
+benchmarkForce action param = do
   let diffTimeInSeconds t1 t2 = realToFrac (t2 `diffUTCTime` t1) :: Double
 
-  param1Eval <- (evaluate . force) param1
-  param2Eval <- (evaluate . force) param2
+  paramEval <- (evaluate . force) param
 
   before <- getCurrentTime
 
   -- Force the first time to measure computation + forcing
-  result <- (evaluate . force) (action param1Eval param2Eval)
+  result <- (evaluate . force) (action paramEval)
 
   after <- getCurrentTime
 
   hPutStrLn stderr $ showFFloat Nothing (diffTimeInSeconds before after) ""
 
   return result
-          
-performOperation :: (Eq sym, Ord sta) => Fa sym sta -> Fa sym sta -> Bool
-performOperation =
-  isSubsetOf
+
+-- main :: IO ()
+-- main = do
+--   arguments <- getArgs
+--   case parseArguments arguments of
+--     Nothing -> putStrLn "Wrong arguments"
+--     Just (filePath1, filePath2) -> do
+--       file1 <- B.readFile filePath1
+--       file2 <- B.readFile filePath2
+--       case parseFa file1 of
+--         Left error -> fail error
+--         Right fa1 ->
+--           case parseFa file2 of
+--             Left error -> fail error
+--             Right fa2 ->
+--                 benchmarkForce Regular.intersect fa1 fa2 >>= print
+
+-- parseArguments :: [String] -> Maybe (FilePath, FilePath)
+-- parseArguments (x:y:_) =
+--   Just (x, y)
+-- parseArguments _ =
+--   Nothing
+
+-- http://stackoverflow.com/questions/18612303/haskell-how-to-benchmark-a-computation-accurately-with-deepseq-force
+
+-- benchmarkForce :: (NFData a, NFData p, NFData r) => (p -> r -> a) -> p -> r -> IO a
+-- benchmarkForce action param1 param2 = do
+--   let diffTimeInSeconds t1 t2 = realToFrac (t2 `diffUTCTime` t1) :: Double
+
+--   param1Eval <- (evaluate . force) param1
+--   param2Eval <- (evaluate . force) param2
+
+--   before <- getCurrentTime
+
+--   -- Force the first time to measure computation + forcing
+--   result <- (evaluate . force) (action param1Eval param2Eval)
+
+--   after <- getCurrentTime
+
+--   hPutStrLn stderr $ showFFloat Nothing (diffTimeInSeconds before after) ""
+
+--   return result
--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -9,37 +9,78 @@ else
   exit 1
 fi
 
-directory="/home/jakub/Desktop/fas"
-csv="/home/jakub/Sources/thesis/data/incl-automata.csv"
+directory="/home/jakub/Desktop/fas-univ"
+csv="/home/jakub/Sources/thesis/data/table/univ-vata.csv"
+
+echo $csv
 
 echo "x,y,z" > $csv
 
 files=$(find $directory -type f | sort)
 
-for file1 in $files
+for file in $files
 do
-  for file2 in $files
-  do
-    # Execute program and save stderr to time.
-    $program $file1 $file2 >> /tmp/results-automata 2> /tmp/error
-    time=$(</tmp/error)
+  # Execute program and save stderr to time.
+  $program "/home/jakub/Desktop/universal-fa.tmb" $file >> /tmp/results-automata 2> /tmp/error
+  time=$(</tmp/error)
 
-    # Get rid of scientific notation
-    time=`echo ${time} | sed -e 's/[eE]+*/\\*10\\^/'`
+  # Get rid of scientific notation
+  time=`echo ${time} | sed -e 's/[eE]+*/\\*10\\^/'`
 
-    # Time in seconds convert to miliseconds
-    time=$(echo "1000 * $time" | bc -l)
+  # Time in seconds convert to miliseconds
+  time=$(echo "1000 * $time" | bc -l)
 
-    states1=$(basename "$file1")
-    states1="${states1%.*}"
+  states=$(basename "$file")
+  states="${states%.*}"
 
-    states2=$(basename "$file2")
-    states2="${states2%.*}"
-
-    printf "%s,%s,%s\n" "$states1" "$states2" "$time" >> $csv
-  
-  done
-
-  echo "" >> $csv
+  printf "%s,%s\n" "$states" "$time"
+  printf "%s,%s\n" "$states" "$time" >> $csv
 
 done
+
+
+# #!/bin/bash
+
+# if [ "$1" == "vata" ]; then
+#   program="vata -t -r expl_fa isect "
+# elif [ "$1" == "automata" ]; then
+#   program="automata-exe "
+# else
+#   echo "Program not supported."
+#   exit 1
+# fi
+
+# directory="/home/jakub/Desktop/fas"
+# csv="/home/jakub/Sources/thesis/data/table/isect-automata.csv"
+
+# echo $csv
+
+# echo "x,y,z" > $csv
+
+# files=$(find $directory -type f | sort)
+
+# for file1 in $files
+# do
+#   for file2 in $files
+#   do
+#     # Execute program and save stderr to time.
+#     $program $file1 $file2 >> /tmp/results-automata 2> /tmp/error
+#     time=$(</tmp/error)
+
+#     # Get rid of scientific notation
+#     time=`echo ${time} | sed -e 's/[eE]+*/\\*10\\^/'`
+
+#     # Time in seconds convert to miliseconds
+#     time=$(echo "1000 * $time" | bc -l)
+
+#     states1=$(basename "$file1")
+#     states1="${states1%.*}"
+
+#     states2=$(basename "$file2")
+#     states2="${states2%.*}"
+
+#     printf "%s,%s,%s\n" "$states1" "$states2" "$time"
+#     printf "%s,%s,%s\n" "$states1" "$states2" "$time" >> $csv
+  
+#   done
+# done
